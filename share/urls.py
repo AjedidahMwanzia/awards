34ddd3aa1964022d539088eef7e95eff0984a0c1
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-11 18:03:00 +0300

    add registration urls in project url

--- share/urls.py
+++ share/urls.py
@@ -20,4 +20,12 @@ from django_registration.backends.one_step.views import RegistrationView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('awards.urls')),
+    path(
+        "accounts/register/",
+        RegistrationView.as_view(success_url="/profile"),
+        name="django_registration_register",
+    ),
+    path("accounts/", include("django_registration.backends.one_step.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
