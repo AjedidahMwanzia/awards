d71a9bea72aca38c907535fb4d4a21a5dbc29a8a
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-11 11:51:25 +0300

    initialcommit

--- share/settings.py
+++ share/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^70afd4i2*!dfcik3-=z9x^asb@pb%jgng5%4es$eup0wn2!i4'
+SECRET_KEY = 'django-insecure-mqa-))8-0s$-1k5+@6_=3j!e%mn3vnq67b_si6ya7y$s07__tn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
