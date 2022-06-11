3a3cfa8cdef49836deb7e058c75fdedf3e7de1b7
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-11 15:36:38 +0300

    update home views

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Profile)
+admin.site.register(models.Post)
+admin.site.register(models.Rating)
+
--- awards/views.py
+++ awards/views.py
@@ -3,4 +3,21 @@ from django.http  import HttpResponse,Http404
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0, len(posts)-1)
+        random_post = posts[a_post]
+        print(random_post.photo)
+    except Post.DoesNotExist:
+        posts = None
+    return render(request, 'home.html', {'posts': posts, 'form': form, 'random_post': random_post})
