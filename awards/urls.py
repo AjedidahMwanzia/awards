48fbbf8bf0daeb09475e693a1014d35a55456b33
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-11 20:29:29 +0300

    handle registration errors

--- awards/urls.py
+++ awards/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, re_path as url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
--- share/settings.py
+++ share/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'awards',
     'bootstrap3',
     'cloudinary',
-    'django-registration',
+    'registration',
   
 ]
 
