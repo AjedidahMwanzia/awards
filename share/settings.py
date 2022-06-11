52e713fbe4251fe0e916de1dd39a5722dcb4b2c4
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-11 18:10:02 +0300

    add search project and project details views

--- share/settings.py
+++ share/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'awards',
     'bootstrap3',
     'cloudinary',
+    'django-registration',
   
 ]
 
--- share/urls.py
+++ share/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf.urls import url, include
 from django_registration.backends.one_step.views import RegistrationView
 urlpatterns = [
     path('admin/', admin.site.urls),
