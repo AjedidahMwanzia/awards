632561e885dd942f3fabc6ea2056e15c9e09fd87
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-11 14:09:14 +0300

    add models.py

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,87 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length = 100, null=False,blank=False)
+
+    def __str__(self):
+        return self.name
+    def save_category(self):
+        self.save()
+    # delete the image database
+    def delete_category(self):
+        self.delete()
+
+    def get_all_category(cls):
+        categories = Category.objects.all()
+        return categories
+
+class Photo(models.Model):
+    name = models.CharField(max_length =30,null=True)
+    category=models.ForeignKey(Category, on_delete=models.SET_NULL,null=True,blank=True)
+    description=models.TextField(null=True)
+    image = models.ImageField(default='DEFAULT VALUE')
+    location = models.ForeignKey('Location',null=True,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.description
+
+    def save_image(self):
+        self.save()
+    # delete the image database
+    def delete_image(self):
+        self.delete()
+    # get all images
+    def update_image(self, name, description, location, category):
+        self.name = name
+        self.description = description
+        self.location = location
+        self.category = category
+        self.save()
+
+    @classmethod
+    def get_all_images(cls):
+        images = Photo.objects.all()
+        return images
+    
+
+    # get image by id
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = Photo.objects.get(id=id)
+        return image
+
+    @classmethod
+    def filter_by_category(cls, category_id):
+        images = Photo.objects.filter(category_id=category_id)
+        return images
+    # get images by location
+    @classmethod
+    def filter_by_location(cls, location_id):
+        images = Photo.objects.filter(location__id=location_id)
+        return images
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        searched_photos=cls.objects.filter(category__name__icontains=search_term)
+        return searched_photos
+
+class Location(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    # save location to database
+    def save_location(self):
+        self.save()
+
+    # update location
+    def update_location(self, name):
+        self.name = name
+        self.save()
+
+     # delete location from database
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+  
--- share/settings.py
+++ share/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config( 
+  cloud_name = "YOUR_CLOUD_NAME", 
+  api_key = "YOUR_API_KEY", 
+  api_secret = "YOUR_API_SECRET" 
+)
