9c6d5f1864f40b7391eba17db22458c9390871d0
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-11 17:54:06 +0300

    update urls for the award app

--- awards/urls.py
+++ awards/urls.py
@@ -4,5 +4,15 @@ from . import views
 
 urlpatterns = [
     path('',views.home ,name= 'home'),
-   
+    path("profile/", views.profile, name="profile"),
+    path("accounts/profile/", views.profile, name="profile"),
+    path("profile/update/", views.update_profile, name="update_profile"),
+    path("project/save/", views.save_project, name="save_project"),
+    path("project/<int:project_id>/", views.project_details, name="project_details"),
+    path("project/delete/<int:id>/", views.delete_project, name="delete_project"),
+    path("project/rate/<int:id>/", views.rate_project, name="rate_project"),
+    path("search/", views.search_project, name="search_project"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
