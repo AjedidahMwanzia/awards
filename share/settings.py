366ed9ebb5e826760948131c767d8e647384b2b7
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-08-09 15:31:21 +0300

    style the cards in homepage

--- share/settings.py
+++ share/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
 
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
